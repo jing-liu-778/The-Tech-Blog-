2872bc5e052e5a49a4b6988c5b1096f1228da041
Jing Liu <jingliu778@gmail.com>
2022-02-12 20:25:38 -0500

    update sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,39 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-let sequelize;
+// let sequelize;
+const sequelize = new Sequelize(
+  "heroku_c928792223d0cda",
+  "b5aae6f64529ac",
+  "0ba463fb",
+  {
+    host: "us-cdbr-east-05.cleardb.net",
+    dialect: "mysql",
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
+
+// const mysql = require("mysql");
+// var connection = mysql.createPool({
+//   host: "us-cdbr-east-05.cleardb.net",
+//   user: "b5aae6f64529ac",
+//   password: "0ba463fb",
+//   database: "heroku_c928792223d0cda",
+// });
+// module.exports = connection;
